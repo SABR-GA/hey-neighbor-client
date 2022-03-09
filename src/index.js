494df1a0efd9ed6d895ab9ba1263cde60375f42d
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-03-09 16:35:46 -0600

    added react component for overlay nav display with commented out search functionality, added footer, set height of main in app to 90 vh to secure footer to the end of the page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom";
-
-
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <HashRouter>
     <App />
