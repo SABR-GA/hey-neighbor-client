b6c8edc0a3cfb6a1b65602b8641acf69fbffed60
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-03-10 18:24:08 -0600

    styled fotoer to stay at the bottom of feed. Styled feed, finished styling newPost

--- src/feed/feed.js
+++ src/feed/feed.js
@@ -9,8 +9,7 @@ const Feed = () => {
       .then((response) => response.json())
       .then((data) => setFeed(data))
       .catch(() => console.log("whoopsie daisy you did something wrong"));
-  
- }, [])
+  }, []);
 
   const displayFeed = feed.map((post) => (
     // <div key={post._id}>
@@ -20,28 +19,26 @@ const Feed = () => {
     //   <h5>{post.Location}</h5>
     //   <h6>{post.Description}</h6>
     // </div>
+    <Link to={`/postPage/${post._id}`} className="feed-post-cards">
+      <Card style={{ width: "18rem" }} key={post._id}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body>
+          <Card.Title>
+            {post.Title}
+            {post.Date}
+          </Card.Title>
+          <Card.Subtitle>{post.Location}</Card.Subtitle>
 
-<Card style={{ width: '18rem' }} key={post._id}>
-{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-<Card.Body>
-  <Card.Title><Link to={`/postPage/${post._id}`}>{post.Title}</Link>{post.Date}</Card.Title>
-  <Card.Text>
-  {post.Location}
-  <br/>
-  {post.Description}
-  <br/>
-    Price: ${post.Price}
-    
-  </Card.Text>
-  {/* <Button variant="primary">Go somewhere</Button> */}
-</Card.Body>
-</Card>
+          <Card.Text>
+            <br />
+            <p>{post.Description}</p>
+            <br />${post.Price}
+          </Card.Text>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
+    </Link>
   ));
-  return (
-    <div className="feed">
-      {displayFeed}
-   
-    </div>
-  );
+  return <div className="feed">{displayFeed}</div>;
 };
-export default Feed
+export default Feed;
